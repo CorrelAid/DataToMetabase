b12c250421f81b3df0e4ec23ff7f947f01b0483b
tuhinmallick <tuhin.mllk@gmail.com>
2022-11-23 10:31:08 +0100

    Metabase client authentication

---
+++ R/metabase_client.R
@@ -0,0 +1,43 @@
+#' Authenticate to Metabase
+#'
+#' Initialize an authenticated session with Metabase.
+#'
+#' @param base_url The base URL of your Metabase server
+#' @param username The username to log in as
+#'
+#' @return Metabaser session
+#'
+#' @examples
+#' mb_session <- metabase_init(base_url, username)
+#'
+#' @export
+metabase_auth <- function () {
+  creds <- list(
+    username = Sys.getenv("METABASE_USER"),
+    password = Sys.getenv("METABASE_PWD")
+  )
+  api_url <- Sys.getenv("METABASE_API_URL")
+  credsAsJSON <- jsonlite::toJSON(creds, auto_unbox=TRUE)
+  req <- httr::POST(paste0(api_url, '/api/session'),
+                    httr::add_headers(
+                      "Content-Type" = "application/json"
+                    ),
+                    body = credsAsJSON
+  )
+
+  if (req$status_code >= 200 && req$status_code < 300) {
+    jsonAuthResponse <- httr::content(req, "text")
+    mb_session_token <- toString(jsonlite::fromJSON(jsonAuthResponse))
+
+    # make sure session is legit
+    mb_session <- list(
+      base_url = api_url,
+      token = mb_session_token
+    )
+    print("Session is authenticated")
+    return(mb_session)
+  } else {
+    print(req$status_code("Session is not authenticated and the error code is: ",req$status_code))
+    return(NULL)
+  }
+}
