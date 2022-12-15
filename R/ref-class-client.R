0870175ce00cac5ccee24a27964da3049f6fcc1e
tuhinmallick <tuhin.mllk@gmail.com>
2022-12-15 21:45:54 +0100

    get_tables

--- R/ref-class-client.R
+++ R/ref-class-client.R
@@ -126,3 +126,14 @@ MetabaseClient$methods(
   }
 )
 
+MetabaseClient$methods(
+  get_tables = function() {
+    tables <- .self$authenticated_get("/table/")
+    do.call(
+      dplyr::bind_rows,
+      lapply(tables, function(data) {
+        list(id = data$id, db_id = data$db_id, name = data$name, db_name = data$db$name)
+      })
+    )
+  }
+)
